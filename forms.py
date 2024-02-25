9b5af30433514580c906ebba16a654e4f0a64ab9
goodbyegirl <mctrinity24@gmail.com>
2024-02-25 18:20:32 +0800

    add CRUD for posts

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, TextAreaField, SubmitField, DateField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from wtforms.fields import EmailField
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, TextAreaField
@@ -26,3 +26,22 @@ class SubscribeForm(FlaskForm):
     class Meta:
         csrf_field_name = 'csrf_token'
         csrf_token_id = 'csrf_token'
+
+class WeatherForm(FlaskForm):
+    city = StringField('City', validators=[DataRequired()])
+    submit = SubmitField('Get Weather')
+
+class BillboardForm(FlaskForm):
+    date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+import json
+from flask import Flask, render_template, redirect, url_for, request, flash, abort
 from werkzeug.utils import secure_filename
 import os
 from dotenv import load_dotenv
 from extensions import db  # Import db from extensions
-from forms import PostForm, SubscribeForm, ContactForm
+from forms import PostForm, SubscribeForm, ContactForm, WeatherForm, BillboardForm, RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask import session
@@ -12,6 +13,15 @@ from werkzeug.exceptions import BadRequest
 import re
 from flask_mail import Message, Mail
 from flask_migrate import Migrate  # Import Flask-Migrate
+from weather_data_weatherstack import get_current_weather
+import subprocess
+from spotify import create_spotify_playlist
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, current_user, LoginManager, UserMixin
+
+
+
+
 
 load_dotenv()
 
@@ -36,9 +46,13 @@ mail = Mail(app)
 csrf = CSRFProtect(app)
 
 db.init_app(app)  # Initialize db with app
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 # Initialize Flask-Migrate after the db has been initialized with app
 migrate = Migrate(app, db)
+
 
 # Import models here, after db has been initialized with app
 class BlogPost(db.Model):
@@ -63,6 +77,25 @@ class Subscriber(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     date_subscribed = db.Column(db.DateTime, default=datetime.utcnow)
 
+# class User(db.Model):
+class User(UserMixin, db.Model):    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def is_admin(self):
+        return self.id == 1  # or other logic to determine admin status
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    
+    
+
 # Helper function to check allowed file types
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
@@ -86,12 +119,21 @@ def update_existing_post_slugs():
         for post in existing_posts:
             # Generate a new slug using the custom slugify function
             new_slug = slugify(post.title)
-            
+
             # Update the post's slug
             post.slug = new_slug
 
         # Commit the changes to the database
         db.session.commit()
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 
 # Routes
 @app.route('/')
@@ -112,7 +154,6 @@ def index():
 
     # Render the index.html template with the necessary data
     return render_template('index.html', form=form, latest_posts=latest_posts, all_posts=all_posts)
-
 
 @app.route('/about')
 def about():
@@ -155,22 +196,52 @@ def contact():
     
     return render_template('contact.html', form=form)
 
+# Register Route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now registered and can log in.')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 # Create Post Route
 @app.route('/create_post', methods=['GET', 'POST'])
 def create_post():
     form = PostForm()
-    if request.method == 'POST':
-        print("Post request received")  # Debugging
-
-    if form.validate_on_submit():
-        print("Form is valid")  # Debugging
+    if request.method == 'POST' and form.validate_on_submit():
         try:
             post_title = form.title.data
             post_content = form.content.data
             post_author = 'Maki'  # Placeholder author
 
             # Generate the slug from the post title
-            slug = slugify(post_title)  # Replace with your custom slugify function
+            slug = slugify(post_title)
 
             # Handle Image Upload
             file = form.post_image.data
@@ -181,18 +252,24 @@ def create_post():
                 file.save(file_path)
                 post_image_path = os.path.join('images/blogimages', filename)
 
-            new_post = BlogPost(title=post_title, content=post_content, slug=slug, author=post_author)
+            new_post = BlogPost(
+                title=post_title,
+                content=post_content,
+                slug=slug,  # Set the slug here
+                author=post_author,
+                post_image=post_image_path
+            )
+
             db.session.add(new_post)
             db.session.commit()
-            print("New post added:", new_post)  # Debugging
-            return redirect(url_for('index'))
+            
+            # Redirect to the view_post page for the newly created post
+            return redirect(url_for('view_post', slug=slug))
         except Exception as e:
             print("Failed to add post:", e)
-    else:
-        print("Form validation failed")  # Debugging
-        print(form.errors)  # Print form errors if validation fails
 
     return render_template('create_post.html', form=form)
+
 
 # Edit Post Route
 @app.route('/edit_post/<int:post_id>', methods=['GET', 'POST'])
@@ -229,6 +306,19 @@ def view_post(slug):
         post.post_image = post.post_image.replace('\\', '/')
     return render_template('view_post.html', post=post)
 
+@app.route('/delete_post/<int:post_id>', methods=['POST'])
+def delete_post(post_id):
+    if not current_user.is_authenticated or current_user.id != 1:
+        abort(403)  # Forbidden access if not admin
+
+    post = BlogPost.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    
+    flash('Post has been deleted successfully.', 'success')
+    return redirect(url_for('gallery'))
+
+
 # Subscribe Route
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
@@ -258,11 +348,68 @@ def subscribe():
         print(e)  # For debugging purposes, log the exception
         return jsonify({'status': 'error', 'message': 'An error occurred. Please try again later.'}), 500
 
+# Function to read city data from the JSON file
+def read_city_data():
+    city_data = []
+
+    with open('static\data\cities.json', 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        for entry in data:
+            city_data.append(entry['city'])
+
+    return city_data
+
+# Route for the "What's Up?" page
+# Modify the 'whatsup' route to handle only weather information
+@app.route('/whatsup', methods=['GET', 'POST'])
+def whatsup():
+    cities = read_city_data()
+    weather_form = WeatherForm()  # Create an instance of the WeatherForm class
+    billboard_form = BillboardForm()  # Create an instance of the BillboardForm class
+
+    if request.method == 'POST':
+        if weather_form.validate_on_submit():
+            city = weather_form.city.data  # Get the inputted city from the form
+
+            # Call your weather data function here, passing the inputted city
+            weather_data = get_current_weather(city)  # Replace with your actual function and API call
+
+            return render_template('whatsup.html', cities=cities, weather_data=weather_data, weather_form=weather_form, billboard_form=billboard_form)
+
+    return render_template('whatsup.html', cities=cities, weather_form=weather_form, billboard_form=billboard_form)
+
+
+# Define the create_playlist route
+# Route to create a Spotify playlist
+@app.route('/create_playlist', methods=['GET', 'POST'])
+def create_playlist():
+    error_message = None
+    success_message = None
+    playlist_url = None
+
+    if request.method == 'POST':
+        date = request.form.get('date')
+
+        # Call the create_spotify_playlist function to create the playlist
+        playlist_url = create_spotify_playlist(date)
+
+        if playlist_url:
+            success_message = "Playlist created successfully!"
+        else:
+            error_message = "Error creating playlist. Please check your input and try again."
+
+    return render_template('create_playlist.html', success_message=success_message, error_message=error_message, playlist_url=playlist_url)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 # Other functions
 
 @app.context_processor
 def inject_csrf_token():
     return dict(csrf_token=lambda: csrf.generate_csrf())
+
 
 
 if __name__ == "__main__":
